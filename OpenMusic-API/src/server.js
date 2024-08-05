ccd281221cdf00b17e35207e1c642ae3f99f3b27
admajarois <rois.admj@gmail.com>
2024-08-05 21:25:59 +0700

    [Dev] add Hapi error handling, should remove try catch in handler

--- OpenMusic-API/src/server.js
+++ OpenMusic-API/src/server.js
@@ -5,6 +5,7 @@ const AlbumsService = require('./services/inMemory/AlbumsService');
 const SongsService = require('./services/inMemory/SongsService');
 const AlbumsValidator = require('./validator/albums');
 const SongsValidator = require('./validator/songs');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
     const albumsService = new AlbumsService();
@@ -33,7 +34,21 @@ const init = async () => {
             service: songsService,
             validator: SongsValidator,
         }
-    })
+    });
+
+    server.ext('onPreResponse', (request, h) => {
+        const { response } = request;
+        if (response instanceof ClientError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: response.message,
+            });
+
+            newResponse.code(response.statusCode);
+            return newResponse;
+        }
+        return h.continue;
+    });
 
     await server.start();
     console.log(`Server running on ${server.info.uri}`);
