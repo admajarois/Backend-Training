eef6eab3dd31ccf6f5c47e37faecb7608e4323a2
admajarois <rois.admj@gmail.com>
2024-11-10 14:55:52 +0700

    add reply interface

---
+++ forum-api/src/Interfaces/http/api/replies/handler.js
@@ -0,0 +1,40 @@
+const AddReplyUseCase = require('../../../../Applications/use_case/replies/AddReplyUseCase');
+const DeleteReplyUseCase = require('../../../../Applications/use_case/replies/DeleteReplyUseCase');
+const autoBind = require('auto-bind');
+
+class RepliesHandler {
+  constructor(container) {
+    this._container = container;
+
+    autoBind(this);
+  }
+
+  async postReplyHandler(request, h) {
+    const { threadId, commentId } = request.params;
+    const { id: owner } = request.auth.credentials;
+    const addReplyUseCase = this._container.getInstance(AddReplyUseCase.name);
+    const addedReply = await addReplyUseCase.execute({ ...request.payload, threadId, commentId, owner });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedReply,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async deleteReplyHandler(request, h) {
+    const { threadId, commentId, replyId } = request.params;
+    const { id: owner } = request.auth.credentials;
+    const deleteReplyUseCase = this._container.getInstance(DeleteReplyUseCase.name);
+    await deleteReplyUseCase.execute({ threadId, commentId, replyId, owner });
+
+    return {
+      status: 'success',
+    };
+  }
+}
+
+module.exports = RepliesHandler;
